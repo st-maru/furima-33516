3b8d720324790827a277cfc5205f7158bacb4c22
st-maru <soma.tamura@gmail.com>
2021-01-06 10:24:34 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :birth_day, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birth_day
+  end
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' } do
     validates :first_name
