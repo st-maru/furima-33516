420ab5b376d0025ae6b1d3b8d53720a2e0c11ba0
st-maru <soma.tamura@gmail.com>
2021-01-07 14:05:08 +0900

    Itemモデルの単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,9 +18,12 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :text
-    validates :price, format: { with: /\A[0-9]+\z/, message: 'Half-width number' }, 
-    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
   end
+
+  # 価格のバリデーション
+  validates :price, presence: true
+  validates :price, numericality: { only_integer: true, message: 'Half-width number' }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
 
   # 初期表示（---)を選択させないためのバリデーション
   with_options numericality: { other_than: 1, message: 'Select'} do
